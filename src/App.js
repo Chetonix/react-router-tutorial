a8151889aeb25bdaabb2f657f7d54bc75adc3a4f
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-07-15 17:10:37 +0530

    add login credentials to the user state and navgigate to dashboard further

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Products from './pages/Products';
 import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
 import SingleProduct from './pages/SingleProduct';
+import { useState }  from 'react';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -14,6 +20,8 @@ function App() {
           <Route path='about' element={<About />} />
           <Route path='products' element={<Products />} />
           <Route path='products/:productId' element={<SingleProduct />} />
+          <Route path='login' element={<Login setUser={setUser} />} />
+          <Route path='dashboard' element={<Dashboard user={user} />} />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
