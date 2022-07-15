4bfadc272f4a921ce6026e000feb4ce4af731cb3
Chetan Harjani <chetonix@Chetans-MacBook-Pro.local>
2022-07-15 14:52:37 +0530

    add nested routing and SharedLayout component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,7 @@
-import { Link, Outlet } from "react-router-dom";
-
-
 const Home = () => {
   return (
     <section className='section'>
       <h2>Home Page</h2>
-      <Outlet />
     </section>
   );
 };
